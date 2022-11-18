0c321f8160c2186c5aad72bd98af2bda4304b86f
José Murillo <jooseemurillo@gmail.com>
2022-11-18 19:44:27 -0300

    apply responsive scales layout in all pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import AppProvider from './hook';
 import Routes from './routes';
 
 import { GlobalStyles } from './styles/global';
+import { ResponsiveScalesLayout } from './styles/global/responsiveScalesLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
-
+      <ResponsiveScalesLayout />
       <AppProvider>
         <Routes />
         <ToastContainer autoClose={3000} />
